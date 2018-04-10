bb11b0c378eae34dfd1fc6dabe854d71f9e59e9c
Dustin Schimel <dustin.schimel@gmail.com>
2018-04-10 13:21:31 -0600

    Completed runner

--- src/art/controller/Runner.java
+++ src/art/controller/Runner.java
@@ -1,6 +1,17 @@
 package art.controller;
 
+import art.controller.ArtController;
+
+/**
+ * Runs the project by calling the PokemonController
+ * @author Dustin Schimel
+ *
+ */
 public class Runner
 {
-
+	public static void main (String [] args)
+	{
+		ArtController app = new ArtController();
+		app.start();
+	}
 }
